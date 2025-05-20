60100b27c8d5477b6b9f400229a988fd1fb518da
Raphaël Chartier <raphaelchartier@free.fr>
2025-05-21 00:03:18 +0200

    chore: set TERM to xterm during install

--- install.sh
+++ install.sh
@@ -4,4 +4,14 @@ sudo apt update
 
 export DOTFILES_DOCKER=1
 export GIT_CLONE_METHOD=https
+export TERM=xterm
+
+if [ -d "$HOME/.cfg" ]; then
+    rm -rf "$HOME/.cfg"
+fi
+
+if [ -d "$HOME/.zshrc" ]; then
+    rm -rf "$HOME/.zshrc"
+fi
+
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/rachartier/dotfiles/main/.config/dot-manager/first_install.sh)"
