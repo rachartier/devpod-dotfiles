82da3002b2e8955f860dcfbf8069ef3c353e14e4
Raphaël CHARTIER <raphael.chartier@michelin.com>
2025-05-21 09:34:33 +0200

    fix: use apt-get instead of apt

--- install.sh
+++ install.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
 if ! [ -d "$HOME/.cfg" ]; then
-    sudo apt update
+    sudo apt-get update
 
     export DOTFILES_DOCKER=1
     export GIT_CLONE_METHOD=https
     export TERM=xterm
 
-    if ! command -v curl &>/dev/null; then
-        sudo apt install -y curl
-    fi
+    sudo apt-get install -y curl
 
     bash -c "$(curl -fsSL https://raw.githubusercontent.com/rachartier/dotfiles/main/.config/dot-manager/first_install.sh)"
 fi
